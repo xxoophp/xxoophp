ba7e8db57d6fc4d0205089808b1f071f86d50a2b
pengbing <214725178@qq.com>
2012-12-31 14:40:19 +0800

    0.2
    0.2

--- xxoophp/index.php
+++ xxoophp/index.php
@@ -46,36 +46,6 @@ define( 'ROOT', dirname(__FILE__).DS );
 // xxoo目录路径
 define( 'XXOO', dirname(__FILE__).DS.'_xxoo'.DS );
 
-// 入口页
-$GLOBALS['xxoo']['index'] = 'index.php';
-
-/*
- * URL协议
- * 根据不同的协议，生成不同的URL，目前支持两种协议方式：
- * pathinfo: 	 PATH_INFO
- * query string: QUERY_STRING
- */
-$GLOBALS['xxoo']['uri_protocol'] = 'PATH_INFO';
-
-// 文件缓存目录
-$GLOBALS['xxoo']['cache_path'] = 'data/cache';
-
-// 上传目录
-$GLOBALS['xxoo']['uploadir'] = 'static/uploads';
-
-// 时区
-$GLOBALS['xxoo']['date_timezone'] = 'Asia/Chongqing';
-
-/* 定义日志输出
- * none: 不启用日志输出
- * console: 输出到eclispe控制台，需要Java支持
- * file: 输出到文件
- * database: 输出到数据库
- */
-define( 'LOG_OUTPUT', 'none' );
-
-// 文件日志目录
-$GLOBALS['xxoo']['logs_dir'] = 'logs';
-
+// 载入框架引导文件
 require( XXOO . '_xxoo.php' );
 
